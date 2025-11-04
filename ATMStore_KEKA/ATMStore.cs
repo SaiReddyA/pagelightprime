276ff109a6d61846fa5c6a0aaacce11eebc93f80
Sai Reddy Local <saireddy1m43@gmail.com>
2025-11-04 14:42:10 +0530

    Keka Task with 4 API's

---
+++ ATMStore_KEKA/ATMStore.cs
@@ -0,0 +1,30 @@
+﻿namespace ATMStore_KEKA
+{
+    public class AddCashRequest
+    {
+        public Dictionary<int, int> Notes { get; set; } // Example: {500:2, 200:1}
+    }
+
+    public class WithdrawRequest
+    {
+        public int Amount { get; set; }
+    }
+
+    public class Transaction
+    {
+        public DateTime Date { get; set; }
+        public string Type { get; set; } // Add or Withdraw
+        public int Amount { get; set; }
+        public string Details { get; set; } // e.g. "500x2, 200x1"
+    }
+    public static class ATMStore
+    {
+        public static Dictionary<int, int> CashNotes = new Dictionary<int, int>()
+        {
+            {20, 0}, {50, 0}, {100, 0}, {200, 0}, {500, 0}, {2000, 0}
+        };
+
+        public static List<Transaction> Transactions = new List<Transaction>();
+    }
+
+}
---
+++ ATMStore_KEKA/Controllers/ATMController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATMStore_KEKA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ATMController : ControllerBase
+    {
+
+        [HttpPost("AddCash")]
+        public IActionResult AddCash([FromBody] AddCashRequest request)
+        {
+            int totalAdded = 0;
+            foreach (var note in request.Notes)
+            {
+                if (ATMStore.CashNotes.ContainsKey(note.Key))
+                {
+                    ATMStore.CashNotes[note.Key] += note.Value;
+                    totalAdded += note.Key * note.Value;
+                }
+            }
+
+            ATMStore.Transactions.Add(new Transaction
+            {
+                Date = DateTime.Now,
+                Type = "Add",
+                Amount = totalAdded,
+                Details = string.Join(", ", request.Notes.Select(x => $"{x.Key}x{x.Value}"))
+            });
+
+            return Ok(new { message = "Cash Added Successfully", totalAdded });
+        }
+
+        [HttpPost("WithdrawCash")]
+        public IActionResult WithdrawCash([FromBody] WithdrawRequest request)
+        {
+            int amount = request.Amount;
+            var notesUsed = new Dictionary<int, int>();
+            var sortedNotes = ATMStore.CashNotes.OrderByDescending(x => x.Key);
+
+            foreach (var note in sortedNotes)
+            {
+                int noteValue = note.Key;
+                int noteCount = note.Value;
+
+                int needed = Math.Min(amount / noteValue, noteCount);
+                if (needed > 0)
+                {
+                    notesUsed[noteValue] = needed;
+                    amount -= needed * noteValue;
+                }
+            }
+
+            if (amount > 0)
+                return BadRequest("Insufficient denominations or cash to dispense this amount.");
+
+            foreach (var note in notesUsed)
+                ATMStore.CashNotes[note.Key] -= note.Value;
+
+            ATMStore.Transactions.Add(new Transaction
+            {
+                Date = DateTime.Now,
+                Type = "Withdraw",
+                Amount = request.Amount,
+                Details = string.Join(", ", notesUsed.Select(x => $"{x.Key}x{x.Value}"))
+            });
+
+            return Ok(new { message = "Cash Withdrawn Successfully", notesUsed });
+        }
+
+        [HttpGet("GetNotesSummary")]
+        public IActionResult GetNotesSummary()
+        {
+            return Ok(ATMStore.CashNotes.Select(x => $"{x.Key}-{x.Value}"));
+        }
+
+        [HttpGet("GetTransactions")]
+        public IActionResult GetTransactions()
+        {
+            return Ok(ATMStore.Transactions);
+        }
+    }
+}
